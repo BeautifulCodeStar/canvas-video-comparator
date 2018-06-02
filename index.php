cf6372b6bed61021c7463aab38a8c686276e4d09
BeautifulCodeStar <BeautifulCodeStar@outlook.com>
2018-06-02 20:47:23 +0200

    fix

--- index.php
+++ index.php
@@ -3,12 +3,20 @@
 <head>
     <meta charset="utf-8">
     <title>Video comparator tool</title>
-  
+    
+    <link rel="stylesheet" id="fontawesome-css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/main.css">
 </head>
 
 <body data-gr-c-s-loaded="true">
     <div class="layout"></div>
+    <div id="over_video">
+        <span class="big-icon" onclick="play()">
+            <i class="fa fa-play"></i>
+        </span>
+        Canvas Video<br>Comparator
+    </div>
 
     <script src="assets/js/index.js" charset="utf-8"></script>
 
