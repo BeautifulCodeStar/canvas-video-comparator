477242b299e82671f0f10e3124dc5e2cb7c7794b
best service <apple930108@gmail.com>
2018-06-03 23:13:28 +0800

    test

--- index.php
+++ index.php
@@ -1,25 +1,97 @@
-<html lang="en" dir="ltr" class="gr__o0_github_io">
+<html>
+    <head>
+        <title>Video comparator tool</title>
+        <link rel="stylesheet" id="fontawesome-css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" type="text/css" media="all">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+      
+        <canvas id="videoMerge" width="1024" height="576">
+        </canvas>
+        
+        <div id="video-compare-container">
+            <video muted autoplay="autoplay" id="rightVideo">
+                
+                <source src="assets/videos/video-1.mp4">    
+            </video>
+            <video muted autoplay="autoplay" id="leftVideo">
+                <source src="assets/videos/video-2.mp4">
+            </video>
+        </div>
 
-<head>
-    <meta charset="utf-8">
-    <title>Video comparator tool</title>
-    
-    <link rel="stylesheet" id="fontawesome-css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" type="text/css" media="all">
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
-</head>
+        <div id="over_video">
+            <span class="big-icon" onclick="play()">
+                <i class="fa fa-play"></i>
+            </span>
+            Canvas Video<br>Comparator
+        </div>
 
-<body data-gr-c-s-loaded="true">
-    <div class="layout"></div>
-    <div id="over_video">
-        <span class="big-icon" onclick="play()">
-            <i class="fa fa-play"></i>
-        </span>
-        Canvas Video<br>Comparator
-    </div>
+        
+        <div id="videoUI"></div>
+    </body>
+    <script>
+        
+        var videoContainer = document.getElementById("video-compare-container"),
+        videoUI = document.getElementById("videoUI"),
+        videoMerge = document.getElementById("videoMerge"),
+        leftVideo = document.getElementById("leftVideo"),
+        rightVideo = document.getElementById("rightVideo"),
+        videoControl = document.createElement("button"),
+        position = 0.5,
+        vidHeight = 576,
+        vidWidth = 1024;
+        mergeContext = videoMerge.getContext("2d");
+        videoContainer.style.display = "none";
+        videoControl.innerHTML = "Play";
+        videoUI.appendChild(videoControl); 
+        videoControl.addEventListener("click", playPause, false);
 
-    <script src="assets/js/index.js" charset="utf-8"></script>
+        function playPause() {
+            if (leftVideo.paused) {
+                videoControl.innerHTML = "Pause";
+                playVids();
+            } else {
+                leftVideo.pause();
+                rightVideo.pause();
+                videoControl.innerHTML = "Play";
+            }
+        }
 
-</body>
+    function playVids() {
+        if (leftVideo.readyState > 3 && rightVideo.readyState > 3) {
+            leftVideo.play();
+            rightVideo.play();
 
+            function trackLocation(e) {
+                position = ((e.pageX - videoMerge.offsetLeft) / videoMerge.offsetWidth);
+                // if (position <= 1 && position >= 0) {
+                //     leftVideo.volume = position;
+                //     rightVideo.volume = (1 - position);
+                // }
+            }
+            
+            videoMerge.addEventListener("mousemove", trackLocation, false); 
+            videoMerge.addEventListener("touchstart",trackLocation,false);
+            videoMerge.addEventListener("touchmove",trackLocation,false);
+
+            function drawLoop() {
+                mergeContext.drawImage(leftVideo, 0, 0, vidWidth, vidHeight,
+                0, 0, vidWidth, vidHeight);
+                    mergeContext.drawImage(rightVideo, 
+                (vidWidth * position).clamp(0.01,vidWidth), 0,
+                (vidWidth - (vidWidth * position)).clamp(0.01,vidWidth),
+                vidHeight,(vidWidth * position).clamp(0.01,vidWidth), 0,
+                (vidWidth - (vidWidth * position)).clamp(0.01,vidWidth), vidHeight);
+                    requestAnimationFrame(drawLoop);
+            }
+            requestAnimationFrame(drawLoop);
+        }
+    }
+
+    Number.prototype.clamp = function(min, max) {
+        return Math.min(Math.max(this, min), max);
+    };
+
+    </script>
 </html>
